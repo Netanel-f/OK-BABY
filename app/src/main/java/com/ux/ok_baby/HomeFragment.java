25b7101870bc27172f0df716d94fce6974e93dba
Aya <aya.jbara@mail.huji.ac.il>
2019-12-30 17:16:51 +0200

    Updated home layout

--- app/src/main/java/com/ux/ok_baby/HomeFragment.java
+++ app/src/main/java/com/ux/ok_baby/HomeFragment.java
@@ -31,6 +31,21 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+    public void setUpOptions(View view) {
+        switch (view.getId()){
+            case R.id.sleepButton:
+                // TODO: 12/30/2019 start an sleep fragment
+            case R.id.diaperButton:
+                // TODO: 12/30/2019 start an diaper fragment
+            case R.id.foodButton:
+                // TODO: 12/30/2019 start an food fragment
+            case R.id.otherButton:
+                // TODO: 12/30/2019 other?
+        }
+    }
+    public void setUpBabyDetails(String name, String age){
+
+    }
 
     
 }
