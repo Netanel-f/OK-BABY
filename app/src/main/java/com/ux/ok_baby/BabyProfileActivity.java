782b36e1a81d23eaebb6defe48cf06cd67f05b64
netanelf <netanelf@cs.huji.ac.il>
2020-01-07 13:00:55 +0200

    changed the conditions of set DOB and Name + some prep for firebase

--- app/src/main/java/com/ux/ok_baby/BabyProfileActivity.java
+++ app/src/main/java/com/ux/ok_baby/BabyProfileActivity.java
@@ -1,9 +1,12 @@
 package com.ux.ok_baby;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,10 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class BabyProfileActivity extends AppCompatActivity {
+
+
+    private final int PROFILE_IMG_REQUEST_CODE = 9239;
+    private final String TAG = "BabyProfileActivity";
 
     // Activity main variables
     private ImageView profile_picture;
@@ -36,6 +43,31 @@ public class BabyProfileActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PROFILE_IMG_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    String filePath = data.getData().getPath();
+                    android.util.Log.d(TAG, "onActivityResult: filepath is " + filePath);
+                    final Uri uri = data.getData();
+                    Log.d(TAG, "Uri: " + uri.toString());
+                    /* TODO:
+                           The next code is needed if we want immediate upload of img to firebase
+                    */
+//                    viewModel.uploadProfileImageToStorage(MainActivity.getCurrentUserID(), uri).observe(getViewLifecycleOwner(), new Observer<String>() {
+//                        @Override
+//                        public void onChanged(String storageUri) {
+//                            loadImage(storageUri);
+//                        }
+//                    });
+                }
+            }
+        }
+    }
+
     private void setupUpdateButton() {
         update_profile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,7 +83,8 @@ public class BabyProfileActivity extends AppCompatActivity {
                 }
                 /* TODO
                     send data to firebase for storage.
-                    Name + DOB + IMAGE
+                    Name + DOB
+                    will we send Image only when pressing update
                  */
             }
         });
@@ -76,32 +109,42 @@ public class BabyProfileActivity extends AppCompatActivity {
     }
 
     private boolean setBabyName() {
-        if (baby_name.getText().toString() != null)//check whether the entered text is not null
+        String baby_name_String = baby_name.getText().toString();
+        // Check whether the entered text is not null and not empty
+        if (baby_name_String != null && !baby_name_String.isEmpty())
         {
             //display the text that you entered in edit text
+            /* TODO:
+                update Firebase with new baby name
+             */
+            return true;
+        } else {
             Toast.makeText(getApplicationContext(), "Enter a baby name", Toast.LENGTH_LONG).show();
             return false;
         }
-        return true;
     }
 
     private boolean setBabyDob() {
-        if (baby_dob.getText().toString() != null) {
+        String baby_dob_String = baby_dob.getText().toString();
+        // Check whether the entered text is not null and not empty
+        if (baby_dob_String != null && !baby_dob_String.isEmpty()) {
+            /* TODO:
+                update Firebase with new baby DOB
+             */
+            return true;
+        } else {
             Toast.makeText(getApplicationContext(), "Enter Valid date of birth", Toast.LENGTH_LONG).show();
             return false;
         }
-        return true;
     }
 
     private void pickProfileImage() {
         Intent myIntent = new Intent(Intent.ACTION_GET_CONTENT, null);
         myIntent.setType("image/*");
         /* TODO:
-            Start Activity for result
-            choose image from device
             upload to Firebase
             return to activity
          */
-        // startActivityForResult(myIntent, PROFILE_IMG_REQUEST_CODE);
+         startActivityForResult(myIntent, PROFILE_IMG_REQUEST_CODE);
     }
 }
