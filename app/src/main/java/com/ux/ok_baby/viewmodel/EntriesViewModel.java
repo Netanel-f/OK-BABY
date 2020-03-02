b4bfcecfefb581d5d80e9628ddab59c1771762e4
netanelf <netanelf@cs.huji.ac.il>
2020-03-02 21:46:45 +0200

    EntriesViewModel coding style

--- app/src/main/java/com/ux/ok_baby/viewmodel/EntriesViewModel.java
+++ app/src/main/java/com/ux/ok_baby/viewmodel/EntriesViewModel.java
@@ -27,50 +27,116 @@ public class EntriesViewModel extends AndroidViewModel {
         this.dbRepo = new FirestoreRepository();
     }
 
+
+    /**
+     * add sleep entry to db
+     * @param bid baby id
+     * @param entry SleepEntry object
+     */
     public void addSleepEntry(String bid, SleepEntry entry) {
         dbRepo.addEntry(Constants.ReportType.SLEEP, bid, entry);
     }
 
+
+    /**
+     * add food entry to db
+     * @param bid baby id
+     * @param entry FoodEntry object
+     */
     public void addFoodEntry(String bid, FoodEntry entry) {
         dbRepo.addEntry(Constants.ReportType.FOOD, bid, entry);
     }
 
+
+    /**
+     * add diaper entry to db
+     * @param bid baby id
+     * @param entry DiaperEntry object
+     */
     public void addDiaperEntry(String bid, DiaperEntry entry) {
         dbRepo.addEntry(Constants.ReportType.DIAPER, bid, entry);
     }
 
+
+    /**
+     * checking if this is the first sleep entry
+     * @return true iff the first sleep entry
+     */
     public boolean isFirstTimeSleep() {
         return isFirstTimeSleep;
     }
 
+
+    /**
+     * set flag of first sleep entry
+     * @param isFirstTimeSleep boolean to save to.
+     */
     public void setIsFirstTimeSleep(boolean isFirstTimeSleep) {
         this.isFirstTimeSleep = isFirstTimeSleep;
     }
 
+
+    /**
+     * Get sleep entries list from DB
+     * @param bid baby id
+     * @return LiveData list of Report Entry
+     */
     public LiveData<List<ReportEntry>> getSleepEntries(String bid) {
         return dbRepo.getSleepEntries(bid);
     }
 
+
+    /**
+     * checking if this is the first food entry
+     * @return true iff the first food entry
+     */
     public boolean isFirstTimeFood() {
         return isFirstTimeFood;
     }
 
+
+    /**
+     * set flag of first food entry
+     * @param isFirstTimeFood boolean to save to.
+     */
     public void setIsFirstTimeFood(boolean isFirstTimeFood) {
         this.isFirstTimeFood = isFirstTimeFood;
     }
 
+
+    /**
+     * Get food entries list from DB
+     * @param bid baby id
+     * @return LiveData list of Report Entry
+     */
     public LiveData<List<ReportEntry>> getFoodEntries(String bid) {
         return dbRepo.getFoodEntries(bid);
     }
 
+
+    /**
+     * checking if this is the first food entry
+     * @return true iff the first food entry
+     */
     public boolean isFirstTimeDiaper() {
         return isFirstTimeDiaper;
     }
 
+
+    /**
+     * set flag of first food entry
+     * @param isFirstTimeDiaper boolean to save to.
+     */
     public void setIsFirstTimeDiaper(boolean isFirstTimeDiaper) {
         this.isFirstTimeDiaper = isFirstTimeDiaper;
     }
 
+
+    /**
+     * Get diaper entries list from DB
+     * @param bid baby id
+     * @return LiveData list of Report Entry
+     */
     public LiveData<List<ReportEntry>> getDiaperEntries(String bid) {
         return dbRepo.getDiaperEntries(bid);
     }
