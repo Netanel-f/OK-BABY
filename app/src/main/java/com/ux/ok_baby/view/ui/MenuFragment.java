5947c830fe045d78b757fbf81bdc51dc5f5c62de
netanelf <netanelf@cs.huji.ac.il>
2020-03-02 21:19:21 +0200

    Menu fragment coding style

--- app/src/main/java/com/ux/ok_baby/view/ui/MenuFragment.java
+++ app/src/main/java/com/ux/ok_baby/view/ui/MenuFragment.java
@@ -5,33 +5,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.ux.ok_baby.R;
 import com.ux.ok_baby.view.ui.reports.ReportsHolderFragment;
 
 import static com.ux.ok_baby.utils.Constants.*;
 
+
 public class MenuFragment extends Fragment implements View.OnClickListener {
     private View view;
     private String babyID;
     private ReportsHolderFragment reportsHolderFragment;
 
-
+    /**
+     * Ctr for menu frame
+     * @param babyID baby's id
+     */
     public MenuFragment(String babyID) {
         this.babyID = babyID;
     }
 
+
+    /**
+     * update baby id
+     * @param babyID new baby id
+     */
     public void updateBabyID(String babyID) {
         this.babyID = babyID;
         if (reportsHolderFragment != null)
             reportsHolderFragment.updateBabyID(babyID);
     }
+
 
     @Nullable
     @Override
@@ -42,16 +50,21 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    /**
+     * this method will setup the bottom bar click listeners
+     */
     private void setUpOptions() {
         view.findViewById(R.id.diaperButton).setOnClickListener(this);
         view.findViewById(R.id.foodButton).setOnClickListener(this);
         view.findViewById(R.id.sleepButton).setOnClickListener(this);
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
 
     @Override
     public void onClick(View view) {
@@ -67,6 +80,7 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
 
     @SuppressLint("ResourceType")
     private void startNewFragment(int reportType) {
